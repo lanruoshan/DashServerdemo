192bcd630b0bb2ac9082774be0c1699376c4bd23
lanruoshan <46854427+lanruoshan@users.noreply.github.com>
2019-01-23 18:38:00 +0000

    Update app.py

--- PycharmProjects/MultipageApp/app.py
+++ PycharmProjects/MultipageApp/app.py
@@ -8,10 +8,10 @@ server = app.server
 app.config.suppress_callback_exceptions = True
 
 # Keep this out of source code repository - save in a file or a database
-VALID_USERNAME_PASSWORD_PAIRS = [
-    ['shell', '12345']
-]
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
-)
+#VALID_USERNAME_PASSWORD_PAIRS = [
+  ###  ['shell', '12345']
+#]
+#auth = dash_auth.BasicAuth(
+ #   app,
+ #   VALID_USERNAME_PASSWORD_PAIRS
+#)
